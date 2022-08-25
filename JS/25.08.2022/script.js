e4e6059bcd8b307a154bea6fbcb559bf3b89d832
Damir <damiryessenbek@gmail.com>
2022-08-25 21:52:45 +0600

    Условные операторы

--- JS/25.08.2022/script.js
+++ JS/25.08.2022/script.js
@@ -76,16 +76,150 @@
 
 // alert(Math.abs(number));
 
-let number = +prompt("Enter your number");
+// let number = +prompt("Enter your number");
 
-if (number > 0 ){
-    alert(number);
-}
-else if (number < 0) {
-    alert(-1*number);
-}
-else{
-    alert(0);
-}
+// if (number > 0 ){
+//     alert(number);
+// }
+// else if (number < 0) {
+//     alert(-1*number);
+// }
+// else{
+//     alert(0);
+// }
 
-// Task four
+// // Task four
+
+// let hours = +prompt("Enter what hour");
+// let minutes = +prompt("Enter what minutes");
+// let seconds = +prompt("Enter what seconds");
+
+// if ((hours > 0 && hours < 24) && (minutes > 0 && minutes < 60) && (seconds > 0 && seconds < 60)){
+//     alert(hours + ":" + minutes + ":" + seconds);
+// }
+// else{
+//     alert("Incorrect");
+// }
+
+// // Task five
+
+// let x = +prompt("Enter x coordiante...");
+// let y = +prompt("Enter y coordiante...");
+
+// if (x > 0 && y >= 0) {
+//     alert(1);
+// }
+// else if (x < 0 && y >= 0){
+//     alert(2);
+// }
+// else if (y < 0 && x <= 0){
+//     alert(3);
+// }
+// else if (y < 0 && x >= 0){
+//     alert(4);
+// }
+// else if(x==0 && y==0){
+//     alert("Center");
+// }
+// else{
+//     alert("Invalid input");
+// }
+
+// // Task two Switch
+
+// let fNumber = +prompt("Enter first number...");
+// let sNumber = +prompt("Enter second number...");
+
+// let sign = prompt("Enter sign...");
+
+// switch(sign){
+//     case "+":
+//         alert(fNumber+sNumber);
+//         break;
+//     case "-":
+//         alert(fNumber-sNumber);
+//         break;
+//     case "*":
+//         alert(fNumber*sNumber);
+//         break;
+//     case "/":
+//         alert(fNumber/sNumber);
+//         break;
+// }
+
+// // task one Ternary
+
+// let fNumber =+prompt("Enter a number");
+// let sNumber =+prompt("Enter a number");
+
+// (fNumber>sNumber)?alert(fNumber):alert(sNumber);
+
+// if(fNumber>sNumber){
+//     alert(fNumber);
+// }
+// else{
+//     alert(sNumber);
+// }
+
+//153 строка и строки между 155-160 равны по смыслу
+
+// Task two
+// let number = +prompt("Enter a number...");
+
+// (number%5==0)?alert(true):alert(false);
+// // условие?выражени1:выражение2
+
+// if (number%5==0){
+//     alert(true);
+// }
+// else{
+//     alert(false);
+// }
+
+//169 строка и строки между 172-177 равны по смыслу
+
+// // Task three
+
+// let word = prompt("Enter your location: ");
+
+// //(word == "Земля" || word == "земля")?alert("Привет, Землянин"):alert("привет, инопланетянин");
+
+// (word.toLowerCase() == "земля")?alert("Привет, Землянин"):alert("привет, инопланетянин");
+
+// // toLowerCase() делает все буквы строчными.
+
+// if(word.toLowerCase() == "земля"){
+//     alert("Привет, Землянин");
+// }
+// else{
+//     alert("привет, инопланетянин");
+// }
+
+// // 187 ~ 191-196
+
+// function check(numberOne, numberTwo){
+//     if(numberOne>numberTwo){
+//         return 1;
+//     }
+//     else if(numberOne<numberTwo ){
+//         return -1;
+//     }
+//     else if(numberOne==numberTwo){
+//         return 0;
+//     }
+//     else{
+//         return "It's not a number.";
+//     }
+// }
+
+// alert(check(2,2));
+
+// function factorial(number){
+//     return number?number*factorial(number-1):1;
+// }
+
+// alert(factorial(5));
+
+// Рекурсия - функция вызывает саму себя внутри себя
+
+alert(Math.PI)
