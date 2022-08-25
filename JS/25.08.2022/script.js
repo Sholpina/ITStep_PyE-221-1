21ad0cda877ac6a59b57979de78bcde22eda2c57
Damir <damiryessenbek@gmail.com>
2022-08-25 19:46:43 +0600

    Условные операторы

---
+++ JS/25.08.2022/script.js
@@ -0,0 +1,71 @@
+// let a = 1;
+
+
+// if(a ==1){
+//     alert("equal 1");
+// }
+// else if (a ==2){
+//     alert("equal 2")
+// }
+// else if (a ==4){
+//     alert("equal 4")
+// }
+// else if (a ==4){
+//     alert("equal 4")
+// }
+// else if (a ==5){
+//     alert("equal 5")
+// }
+// else if (a ==6){
+//     alert("equal 6") 
+// }
+
+
+// switch(a){
+//     case 1:
+//         alert("equal 1");
+//         break;
+//     case 2:
+//         alert("equal 2");
+//         break;
+//     case 3:
+//         alert("equal 3");
+//         break;
+//     case 4:
+//         alert("equal 4");
+//         break;
+//     case 5:
+//         alert("eqaul 5");
+//         break;
+//     case 6:
+//         alert("equal 6");
+//         break;
+// }
+
+// // Task one
+
+// let number = +prompt("Enter your number: ");
+
+// if(number > 0){
+//     alert("positive");
+// }
+// else if(number < 0){
+//     alert("negative");
+// }
+// else if(number == 0){
+//     alert("Zero");
+// }
+// else{
+//     alert("It's not a number");
+// }
+
+// // Task two
+
+let age = +prompt("Enter your age");
+
+if (age > 0 && age < 120){
+    alert("Correct");
+}
+else{
+    alert("Invalid age");
+}
