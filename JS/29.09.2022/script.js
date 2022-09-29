1d8d31508f5496456c959a172e93a7006534db86
Damir <damiryessenbek@gmail.com>
2022-09-29 20:19:55 +0600

    Строки

--- JS/29.09.2022/script.js
+++ JS/29.09.2022/script.js
@@ -1,6 +1,6 @@
-let str1 = 'single quoted';
-let str2 = "double quoted";
-let str3 = `backticks`;
+// let str1 = 'single quoted';
+// let str2 = "double quoted";
+// let str3 = `backticks`;
 
 // let number = 7;
 
@@ -20,4 +20,56 @@ let str3 = `backticks`;
 
 // alert(str3[15]);  вывод undefined
 // alert(str3.charAt(15)); вывод пустая строка ''
+
+// toUpperCase = с asdasd в ASDASD
+// toLowerCase = с ASDASD в asdasd
+
+let str = "I hope you understand this description of this"; 
+
+// let target = "h";
+
+// let arr = [];
+// let pos = -1; // Новая позиция точки поиска
+
+// while(true){
+//     let foundPos = str.indexOf(target, pos); // ищет подстроку в заданной позиции
+//     if(foundPos == -1) break; // если подстрока не найдена то он обрывает цикл. -1 означает подстрока не найдена
+//     alert(foundPos); // вывод позиции подстроки
+//     arr.push(foundPos); // добавляет позицию подстроки в наш созданный массив
+//     pos = foundPos + 1; // перезаписывает позицию на 1 больше чем последняя позиция вхожденя подстроки
+// }
+
+// alert(arr);
+
+// for(let i = 0; i < str.length; i++){
+//     if(str[i] == target){ 
+//         arr.push(i)
+//         alert(i);
+//     }
+// }
+
+// while((pos = str.indexOf(target, pos + 1)) != -1){
+//     arr.push(pos);
+// }
+
+// alert(arr);
+
+// alert(str.slice(2,10)) // hope you *слева направо
+// alert(str.slice(-4,-1)) // thi *справа налево
+// alert(str.slice(10,2)) // пустая строка ''
+
+
+// alert(str.substring(2, 10)) = alert(str.substring(10, 2))
+// alert(str.substring(2, 10)); // hope you
+
+// alert(str.substr(2, 10)); // hope you u |10 это количество выводимых символов
+// alert(str.substr(-10, 4)); // on o
+
+// alert(str.substring(-10, -6)); // пустая строка '' | на substring не работают отрицательные числа
+
+// alert("a".codePointAt(0)); //97
+// alert("A".codePointAt(0)); //65
+
+// alert(String.fromCodePoint(97)); // a
+// alert(String.fromCodePoint(65)); // A
 
