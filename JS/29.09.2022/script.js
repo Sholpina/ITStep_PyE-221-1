a99aab6941bf9241515594f71d0285e84e433c8b
Damir <damiryessenbek@gmail.com>
2022-09-29 19:28:58 +0600

    Строки

---
+++ JS/29.09.2022/script.js
@@ -0,0 +1,23 @@
+let str1 = 'single quoted';
+let str2 = "double quoted";
+let str3 = `backticks`;
+
+// let number = 7;
+
+// alert(`I can use in String ${str1}, ${str2} and ${str3}. And let's add a number = ${number}`);
+
+// let str4 = `My students: 
+// 1. Danat
+// 2. Elena
+// 3. Saadat
+// 4. Sholpan
+// `;
+
+// alert(str4);
+
+// alert(str3.length);
+// str3[0] = str3.charAt(0)
+
+// alert(str3[15]);  вывод undefined
+// alert(str3.charAt(15)); вывод пустая строка ''
+
