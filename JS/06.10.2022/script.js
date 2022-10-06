47e9dd235a30f6f6d6abba57c4173bf73a74e256
Damir <damiryessenbek@gmail.com>
2022-10-06 20:34:03 +0600

    Взаимодействие с HTML. Создание элементов. ToDo List

--- JS/06.10.2022/script.js
+++ JS/06.10.2022/script.js
@@ -1,62 +1,60 @@
-// Кнопка закрытия для имеющихся li
+// Добавляем кнопку закрытия для существующих li
 
-let myNodeList = getElementByTagName('li');
-for(let i = 0; i < myNodeList.length; i++){
+let myNodeList = document.getElementsByTagName('li');
+for(let i = 0; i < myNodeList.length; i++) {
     let span = document.createElement('span');
     let txt = document.createTextNode("\u00D7");
-    span.className = 'close';
+    span.className = "close";
     span.append(txt);
-    myNodeList[i].append(span);
+    myNodeList[i].appendChild(span);
 }
 
 
-// Добавлем функцию удаления
+// Добавляем функцию удаления
 
-let close = document.getElementsByClassName("close"); // ищем элементы с классом close
-for(let i = 0; i < close.length; i++){
-    close[i].onclick = function(){
+let close = document.getElementsByClassName('close');
+for(let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
         let div = this.parentElement;
         div.style.display = "none";
     }
 }
 
 
-// Добавляем проверку checked
+// Добавляем проверку checked 
 
-let list = document.querySelector('ul') //  мы ищем элементы с тегом ul
-list.addEventListener('click', function(e) {
-    if(e.target.tagName === 'LI') {
-        e.target.classList.toogle('checked');
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev){
+    if(ev.target.tagName === "LI"){
+        ev.target.classList.toggle('checked');
     }
 }, false);
 
 
-// Создание элемента
+// Создание нового элемента
 
 function newElement(){
-    let li = document.createElement('li'); // создаем элемент с тего li
-    let input = document.getElementById('myInput'); // ищем элемент с id myInput
-    let t = document.createTextNode(input.value); // значение нашего input переводим в текстовое значение
-    li.append(t); // добавялем наше значение внутрь тега li
-    if(input.value==""){ 
-        alert("Please enter something");
-    }
-    else{
+    let li = document.createElement('li');
+    let inputValue = document.getElementById('myInput').value;
+    let t = document.createTextNode(inputValue);
+    li.append(t);
+    if (inputValue === ''){
+        alert('Please enter something!');
+    }else{
         document.getElementById('myUl').append(li);
     }
-    input.value = "";
-
-    let span = document.createElement("span"); // создаем элемент с тегом span
-    let txt = document.createTextNode("\u00D7"); // создаем текстовый узел 
-    span.className = "close"; // присваиваем класс close элементу span
-    span.append(txt); // добавляем текст внутрь span
-    li.append(span); // добавляем span внутрь li
-
+    document.getElementById('myInput').value = "";
     
-    for(let i=0; i<close.length; i++){ // пробегаемся по HTML коллекции
-        close[i].onclick = function(){ // создаем функцию которая будет удалять наш элемент при клике на span
-            let div = this.parentElement; // обращаемся к parent элементу
-            div.style.display = "none"; // удаляем наш элемент сделав его неотображаемым
+    let span = document.createElement('span');
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.append(txt);
+    li.append(span);
+
+    for(let i = 0; i < close.length; i++){
+        close[i].onclick = function(){
+            let div = this.parentElement;
+            div.style.display = "none";
         }
     }
 }
