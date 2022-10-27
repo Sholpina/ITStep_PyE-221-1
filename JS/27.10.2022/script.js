bbe27a81cfa5f552fb7ffb7f00c3353eb189b8f0
Damir <damiryessenbek@gmail.com>
2022-10-27 19:21:06 +0600

    Cookies. Local storage

---
+++ JS/27.10.2022/script.js
@@ -0,0 +1,20 @@
+// localStorage.setItem('name', 'Asd');
+// localStorage.setItem('surname', 'Vivi');
+
+// // // localStorage.name = 'damir'
+
+// // // console.log(localStorage.name);
+
+// console.log(localStorage.getItem('name')); // Asd;
+// console.log(localStorage.getItem('surname')); // Vivi;
+
+// localStorage.clear();
+
+// // localStorage.removeItem('name');
+
+// console.log(localStorage.getItem('surname')); // 
+// console.log(localStorage.getItem('name')); // 
+
+// sessionStorage.setItem('full', 'Name');
+
+console.log(sessionStorage.getItem('full'));
